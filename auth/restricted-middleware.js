5021b57aac9700cb6b514953515d4fa817b46651
Rasha Moumneh <rasha@fastmail.com>
2019-12-17 13:17:06 -0500

    refactor resricted middleware

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,24 +1,30 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
-const Users = require('../users/users-model.js');
+const Users = require("../users/users-model.js");
 
 module.exports = (req, res, next) => {
-  const { username, password } = req.headers;
+  // const { username, password } = req.headers;
 
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: 'Ran into an unexpected error' });
-      });
+  // if (username && password) {
+  //   Users.findBy({ username })
+  //     .first()
+  //     .then(user => {
+  //       if (user && bcrypt.compareSync(password, user.password)) {
+  //         next();
+  //       } else {
+  //         res.status(401).json({ message: 'Invalid Credentials' });
+  //       }
+  //     })
+  //     .catch(error => {
+  //       res.status(500).json({ message: 'Ran into an unexpected error' });
+  //     });
+  // } else {
+  //   res.status(400).json({ message: 'No credentials provided' });
+  // }
+  // do i have a session, and do i have a user on that session (imp because session could be used for things other than authentication). The reason we don't just do if (req.session.user) is that we would get undefined if there is no session to begin with
+  if (req.session && req.session.user) {
+    next();
   } else {
-    res.status(400).json({ message: 'No credentials provided' });
+    res.status(400).json({ message: "No credentials provided" });
   }
 };
