7c403c1b007e3dd715e54992d7f4f85ba03903dd
Rasha Moumneh <rasha@fastmail.com>
2019-12-17 14:53:06 -0500

    logout

--- api/server.js
+++ api/server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const sessions = require("express-session"); // <<<<< install express-session
+const knexSessionStore = require("connect-session-knex")(sessions); // to store sessions in database. Function currying. knexSessionStore will return a function and then we pass to it sessions as an argument
 const authRouter = require("../auth/auth-router.js");
 const usersRouter = require("../users/users-router.js");
+const knex = require("../database/dbConfig");
 const server = express();
 const sessionConfiguration = {
   // session storage options
@@ -11,6 +13,18 @@ const sessionConfiguration = {
   secret: "keep it secret, keep it safe!", // used for encryption (must be an environment variable)
   saveUninitialized: true, // has implications with GDPR laws
   resave: false,
+
+  // how to store the sessions
+  store: new knexSessionStore({
+    // do not forget the new keyword - this is a constructor function
+    knex, // imported from dbConfig.js
+    createtable: true,
+    clearInterval: 1000 * 60 * 10, // defaults to 60000
+    //optional
+    tablename: "sessions"
+    // sidfieldname: "id"
+  }),
+
   // cookie options
   cookie: {
     maxAge: 1000 * 60 * 10, // 10 mins in milliseconds
